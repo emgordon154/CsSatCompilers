a087eb09039d00e3be2b980829dce1c6493066aa
Emily Gordon <emgordon154@gmail.com>
2017-11-04 14:51:59 -0400

    finished(?????) lexer

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 class Calculator {
   constructor (inputString){
     this.tokenStream = this.lexer(inputString);
-  }
+  } 
   lexer(inputString) {
     var makeESLintHappy = this;
     var tokenStream = [],
@@ -14,13 +14,55 @@ class Calculator {
       ["LPAREN",    /^\(/  ],
       ["RPAREN",    /^\)/  ]
     ];
-    for (let i=0; i< inputString.length; i++){
-      let char = inputString[i];
-      for (let j=0; j<tokenTypes.length; j++) {
-        let [token, regEx] = [tokenTypes[j][0], tokens[j][1]];
-        if (regEx.test(char)) tokenStream.push({'name': token, 'value': char})
-      }
+    
+    while(inputString.length) {
+      tokenTypes.forEach(function(tokenType) {
+        let [token, regEx] = [tokenType[0], tokenType[1]];
+        let match = regex.exec(inputString)
+        if (match) {
+          tokenStream.push({'name': token, 'value': match[0]})
+        };
+      })
     }
+
     return tokenStream
   }
-} //1 + 2 * (3+4) => [1, +, 2, *, (, +, 4, )]
+} //1 + 2 * (3+4) => [1, +, 2, *, (, +, 4, )] 
+
+
+
+
+
+// lexer(inputString) {
+//   var makeESLintHappy = this;
+//   var tokenStream = [],
+//   tokenTypes =[
+//     ["NUMBER",    /^\d+/ ],
+//     ["ADD",       /^\+/  ],
+//     ["SUB",       /^\-/  ],
+//     ["MUL",       /^\*/  ],
+//     ["DIV",       /^\//  ],
+//     ["LPAREN",    /^\(/  ],
+//     ["RPAREN",    /^\)/  ]
+//   ];
+//   for (let i=0; i< inputString.length; i++){
+//     let char = inputString[i];
+//     for (let j=0; j<tokenTypes.length; j++) {
+//       let [token, regEx] = [tokenTypes[j][0], tokens[j][1]];
+//       if (regEx.test(char)) {
+//         tokenStream.push({'name': token, 'value': char});
+//         break
+//       }
+//     }
+//   }
+
+//   for (let i=0; i<tokenStream.length; i++) {
+//     if (tokenStream[i].name == 'NUMBER') {
+//       if tokenStream[i+1].name == 'NUMBER') {
+//         tokenStream[i].value =
+//       }
+//     }
+//   }
+
+//   return tokenStream
+// }
